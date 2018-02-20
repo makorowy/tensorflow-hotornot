8a91a3530240691f9a548d942625c143afadf19e
Makor <mateusz.budzar@droidsonroids.pl>
2018-02-20 07:33:56 +0100

    take a picture on start

--- app/src/main/java/com/makor/hotornot/MainActivity.kt
+++ app/src/main/java/com/makor/hotornot/MainActivity.kt
@@ -1,12 +1,34 @@
 package com.makor.hotornot
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.provider.MediaStore
 import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+
+private const val REQUEST_TAKE_PICTURE = 1
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        takePicture()
+    }
+
+    private fun takePicture() {
+        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        if (takePictureIntent.resolveActivity(packageManager) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_TAKE_PICTURE)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        val extras = data?.extras
+        if (requestCode == REQUEST_TAKE_PICTURE && resultCode == Activity.RESULT_OK && extras != null) {
+            Toast.makeText(this, "Picture taken!", Toast.LENGTH_SHORT).show()
+        }
     }
 }
