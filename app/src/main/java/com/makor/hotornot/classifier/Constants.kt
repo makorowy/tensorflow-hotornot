41ab3fd533a2e19a3c819c98aff1599f1edfd061
Makor <mateusz.budzar@droidsonroids.pl>
2018-05-28 19:49:38 +0200

    Replace graph to newly retrained and change input name

--- app/src/main/java/com/makor/hotornot/classifier/Constants.kt
+++ app/src/main/java/com/makor/hotornot/classifier/Constants.kt
@@ -3,7 +3,7 @@ package com.makor.hotornot.classifier
 const val GRAPH_FILE_PATH = "file:///android_asset/graph.pb"
 const val LABELS_FILE_PATH = "file:///android_asset/labels.txt"
 
-const val GRAPH_INPUT_NAME = "input"
+const val GRAPH_INPUT_NAME = "Placeholder"
 const val GRAPH_OUTPUT_NAME = "final_result"
 
 const val IMAGE_SIZE = 224
